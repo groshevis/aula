23f86c0d4b31bcc800c69f05abd85414c6cdb4ba
Aluno <groshevis_eloisa@outlook.com>
2020-03-02 09:33:23 -0300

    site de noticias dia 02/03

---
+++ aula02.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ola ola</title>
+  </head>
+  <body>
+
+    <style media="screen">
+      h1 {
+        background: LightGray;
+      }
+    </style>
+
+    <div class="noticias">
+
+
+      <h1> Paredão desta semana é fomado por: Guilherme, Pyong e Gisele! </h1>
+      <img src="https://pbs.twimg.com/media/ESEpmtTWsAESWZ5.jpg" width="400" height="200">
+      <p>Felipe como anjo inicialmente imunizou Flayslane.</p>
+      <p>Indicado pela líder Ivy, Guilherme foi para o paredão e ganhou o benefício de indicar um brother para a prova de bate-volta, e com isso ele indicou Pyong.</p>
+      <p>Felipe foi o participante mais votado no Confessionário.
+    Ele recebeu seis votos. Com isso, o arquiteto teve que indicar mais um participante para a Prova Bate e Volta. Felipe escolheu Gizelly.</p>
+
+
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    </div>
+
+    <div class="noticias">
+    <h1>Não sabe como escolher um cachorro ideal? Nós te ajudamos!</h1>
+    <p>Não sabe que cachorro ter na sua casa, sem problemas, a tabela abaixo irá te ajudar1</p>
+    <p>Abaixo são descritos os principais atributos que um cão deve ter!</p>
+
+    <table>
+  <tr>
+    <th>Raça</th>
+    <th>Beleza</th>
+    <th>Companherismo</th>
+    <th>Resultado</th>
+
+  </tr>
+  <tr>
+    <td>Pitbull</td>
+    <td>0,1</td>
+    <td>0</td>
+    <td> Não vale a pena </td>
+  </tr>
+  <tr>
+    <td>Vira lata caramelo</td>
+    <td>10 000</td>
+    <td>10 000</td>
+     <td> Excelente escolha </td>
+  </tr>
+  <tr>
+    <td>Pinscher</td>
+    <td>0</td>
+    <td>0</td>
+     <td> Não vale a pena </td>
+  </tr>
+  <tr>
+    <td>Chow Chow</td>
+    <td>0,3</td>
+    <td>0</td>
+     <td> Não vale a pena </td>
+  </tr>
+
+</table>
+  <p>Não compre, adote!</p>
+    </div>
+
+    <div class="noticias">
+
+      <h1>Patrimônio histórico brasileiro: vira lata caramelo</h1>
+      <img src="https://www.hypeness.com.br/wp-content/uploads/2019/09/Vira-lata_Caramelo_1.jpg">
+      <p>Considerado por todos os brasileiros como patrimônio histórico, o vira lata caramelo é definido por mim, o melhor cachorro existente.</p>
+      <p>Com ótimas características como: fiel, bonito, feliz, brincalhão. Concordamos que não há motivo para não amar um.</p>
+      <p>Se você leitor que está lendo isso, não gosta de cachorro e nem do vira lata caramelo, favor nunca dirigir a palavra à mim.</p>
+    </div>
+
+
+  </body>
+</html>
