929745a71e564fda04704b82760d6d440c6f031c
groshevis <groshevis_eloisa@outlook.com>
2020-03-09 09:56:03 -0300

    aula

---
+++ aula04.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+
+    <!-- primeiro importar a fonte -->
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata|Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula04.css">
+  </head>
+
+
+  <body>
+
+    <div class="site">
+
+        <div class="topo">
+          Topo do site
+        </div>
+
+        <div class="conteudo">
+          Corpo do site
+        </div>
+
+        <div class="rodape">
+          Desenvolvido por Eloisa Groshevis
+        </div>
+
+    </div>
+
+  </body>
+
+</html>
